dd42dcf19ba18ee580e5a623178d9de3db9d1f32
Moses Hunsu <hunsumoses7@gmail.com>
2023-09-07 15:03:11 +0100

    Implemented Unit test for createEmployee REST API

---
+++ src/main/java/net/higherAchievers/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package net.higherAchievers.controller;
+
+import net.higherAchievers.entity.Employee;
+import net.higherAchievers.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+}
---
+++ src/test/java/net/higherAchievers/controller/EmployeeControllerTests.java
@@ -0,0 +1,63 @@
+package net.higherAchievers.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.higherAchievers.entity.Employee;
+import net.higherAchievers.service.EmployeeService;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebMvcTest
+public class EmployeeControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    // JUnit test for create employee REST API
+    @DisplayName("JUnit test for create employee REST API")
+    @Test
+    public void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws Exception {
+
+        // given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("Moses")
+                .lastName("Hunsu")
+                .email("moses@gmail.com")
+                .build();
+
+        given(employeeService.saveEmployee(ArgumentMatchers.any(Employee.class)))
+                .willAnswer(invocation -> invocation.getArgument(0));
+
+        // when - action or the behaviour to be tested
+        ResultActions response = mockMvc.perform(post("/api/employees")
+                .contentType(APPLICATION_JSON) //APPLICATION_JSON from MediaType
+                .content(objectMapper.writeValueAsString(employee)));
+
+        // then - verify the output
+        response.andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName", CoreMatchers.is(employee.getFirstName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", CoreMatchers.is(employee.getLastName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(employee.getEmail())));
+
+    }
+
+}
